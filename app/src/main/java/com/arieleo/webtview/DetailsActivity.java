819bfa105596f0f91e927772aa603c9ea56c8f71
chenliang0571 <chenliang0571@gmail.com>
2021-01-05 13:37:34 +0000

    generated code

---
+++ app/src/main/java/com/arieleo/webtview/DetailsActivity.java
@@ -0,0 +1,22 @@
+package com.arieleo.webtview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/*
+ * Details activity class that loads LeanbackDetailsFragment class
+ */
+public class DetailsActivity extends Activity {
+    public static final String SHARED_ELEMENT_NAME = "hero";
+    public static final String MOVIE = "Movie";
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+
+}
